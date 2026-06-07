ef3ef2322097bc7d7fc7a6e1a6c8bd97c98c84e7
Oh yeah jr <anniehuang2022531@gmail.com>
2026-06-07 01:28:45 +0000

    finished lesson 1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,11 @@
+print ("hello!")
+print ("my name is Annie!")
+print ('my favrote food is fried rice')
+
+print ("my favrote animal are humans!")
+# print ("I love being a human!')
+print ("67")
+a = 2 
+print (a)
+thing = human
+print ("my favrote animal are," thing!")
